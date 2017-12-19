c5eb86e1e7d682716cb54077e629ff0a72c189f3
Sergey Alipchenkov <therox@therox.havana>
2017-12-19 09:30:27 +0300

    Задача 12 - удвоение символов, кроме '

---
+++ 12.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Введите, пожалуйста, строку:";
+    string text;
+    getline(std::cin, text);
+    // string text = "Это должно входить, это не, должно, НЕТ, ДА, ДА, НЕТ, НЕТ, НЕТ , -., последовательность из 5 букв А: ААААА''', и это не должно, YXYXYЙ, а это должно остаться09";
+
+    std::cout << "Строка на входе: " << text << std::endl;
+
+    // Задача про преобразование строки путем удвоения любого символа кроме "'"
+    string doubleString = "";
+
+    for (size_t j = 0; j < text.length();)
+    {
+        int cplen = 1;
+        if ((text[j] & 0xf8) == 0xf0)
+            cplen = 4;
+        else if ((text[j] & 0xf0) == 0xe0)
+            cplen = 3;
+        else if ((text[j] & 0xe0) == 0xc0)
+            cplen = 2;
+        if ((j + cplen) > text.length())
+            cplen = 1;
+        // Текущий символ text.substr(i, cplen)
+        string currentChar = text.substr(j, cplen);
+
+        // Задача про удвоение символов кроме "'"
+        // Если текущий символ - '
+        if (currentChar == "'")
+        {
+            // то просто добавляем его к результирующей строке
+            doubleString += currentChar;
+        }
+        else
+        {
+            // иначе, добавляем к результирующей строке 2 текущих символа
+            doubleString += currentChar + currentChar;
+        }
+
+        j += cplen;
+    }
+
+    std::cout << "Задача 12: "
+              << "Результат: " << doubleString << std::endl;
+
+    return 0;
+}
